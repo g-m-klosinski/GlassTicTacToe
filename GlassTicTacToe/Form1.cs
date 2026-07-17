84865659d61f080a18acaac0e0ff23a856fefd32
Grzegorz Klosinski <g.m.klosinski.wut@gmail.com>
2026-07-17 14:12:43 +0200

    Add placing pieces

--- GlassTicTacToe/Form1.cs
+++ GlassTicTacToe/Form1.cs
@@ -1,10 +1,38 @@
+using Microsoft.VisualBasic;
+
 namespace GlassTicTacToe
 {
     public partial class Form1 : Form
     {
+        private bool xToMove = true;
+        
         public Form1()
         {
             InitializeComponent();
+
+            for (var i = 0; i < grid.RowCount; i++)
+            {
+                for (int j = 0; j < grid.ColumnCount; j++)
+                {
+                    Label label = new();
+
+                    label.Dock = DockStyle.Fill;
+                    label.TextAlign = ContentAlignment.MiddleCenter;
+                    label.Click += Label_Click;
+
+                    grid.Controls.Add(label, j, i);
+                }
+            }
+        }
+
+        private void Label_Click(object? sender, EventArgs e)
+        {
+            if (sender is Label piece && piece.Text.Length == 0)
+            {
+                piece.Text = xToMove ? "X" : "O";
+
+                xToMove = !xToMove;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A 3x3 tic-tac-toe game for two players.
 
 Roadmap
 -------
-The game is non-functioning yet and has the following roadmap:
+The game is already functioning and has the following roadmap:
 
 - [x] Display empty board
-- [ ] Add placing pieces
+- [x] Add placing pieces
 - [ ] Detect a win
 - [ ] Enhance graphics
